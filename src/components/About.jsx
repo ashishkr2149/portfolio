6fda12490447da92070689cd923d860631a8ae39
Ashish <ashishkr.2149@gmail.com>
2023-08-26 23:27:08 +0530

    Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,12 +40,12 @@ const About = () => {
         variants = { fadeIn("", "", 0.1, 1) }
         className = "mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
         >
-        I'm a skilled software developer with experience in
-        TypeScript and JavaScript, and expertise in frameworks like React,
-        Node.js, and Three.js. I'm a quick learner and collaborate closely
-        with clients to create efficient, scalable and user-friendly solutions
-        that solve real-world problems. Let's work together to bring your ideas
-        to life!
+        A passionate Web Developer with keen knowledge in Frontend Development, 
+        Backend Development as well as MERN 
+        Stack Development and along with that has experience in JavaScript, 
+        and expertise in frameworks like React.js and Node.js. 
+        Being a quick learner I am adept to work in a fast pace 
+        environment dfor the sole purpose of bringing life to ideas.
       </motion.p>
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
