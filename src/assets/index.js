9a579655266d5cd53c93eb65488ecd0e60e8a9bf
Ashish <ashishkr.2149@gmail.com>
2024-04-12 12:55:59 +0530

    Updating Experiences

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,6 +26,7 @@ import threejs from "./tech/threejs.svg";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
+import sggsc from "./company/sggsc.png";
 import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
@@ -59,6 +60,7 @@ export {
   meta,
   shopify,
   starbucks,
+  sggsc,
   tesla,
   carrent,
   jobit,
